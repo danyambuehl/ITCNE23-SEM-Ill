6fcfa54dc6484a9c1ecaa2bfd58ada0b23cc021b
Ambuehl Dany <ambuehld@gmail.com>
2024-05-14 18:55:38 +0200

    mermaid

--- _includes/mermaid_config.js
+++
@@ -1,7 +0,0 @@
-// _includes/mermaid_config.js
-mermaid.initialize({
-    startOnLoad: true,
-    theme: 'default', // or 'dark', 'forest', etc.
-    // Additional configurations can be added here
-  });
-  
